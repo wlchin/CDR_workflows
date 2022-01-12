05c40e25d155485f4dc3801432d74e39bf9437bd
melvin_chin <weeloong.chin@outlook.com>
2022-01-12 19:33:20 +0000

    updated vars

--- Tcells/scripts/sade_felman_parse_datamatrix.py
+++ Tcells/scripts/sade_felman_parse_datamatrix.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
-
 import anndata as ad
 import pandas as pd
 import glob
 import os
 
-humnum = pd.read_csv(snakemake.input[0])["Cell Name"]
+# get list of CD8 T cells
+CD8Tcells = pd.read_csv(snakemake.input[0])["Cell Name"]
 
-# required to get the rows
-unos = pd.read_csv(snakemake.input[2], 
+# parse raw file to get colnames
+rawfile = pd.read_csv(snakemake.input[2], 
             nrows = 0, skiprows=0,
             engine = "python", 
            sep = "\t",
            index_col=False)
-listname = unos.columns.to_list()
-listname.pop(0)
+colnames_of_raw_datafile = rawfile.columns.to_list()
+colnames_of_raw_datafile.pop(0)
 
 
 chunksize = 1000
@@ -27,18 +25,17 @@ with pd.read_csv(snakemake.input[1], chunksize=chunksize, sep = '\t', header = N
         a = chunk.drop(16292, axis = 1)
         a.index.name = "genes"
         a.astype('float64')
-        a.columns = listname
-        selecto = a.columns[a.columns.isin(humnum)]
-        uiu = a[selecto]
-        fn = "test" + str(i) + ".csv"
-        uiu.to_csv(fn)
+        a.columns = colnames_of_raw_datafile
+        select_relevant_cells = a.columns[a.columns.isin(CD8Tcells)]
+        processed_chunked = a[select_relevant_cells]
+        chunk_filenamefilename = "chunk" + str(i) + ".csv"
+        processed_chunked.to_csv(chunk_filename)
 
-
-hum = glob.glob("*.csv")
-new = pd.concat([pd.read_csv(i) for i in hum])
+csv_chunks = glob.glob("*.csv")
+new = pd.concat([pd.read_csv(i) for i in csv_chunks])
 new = new.set_index("genes")
 
-[os.remove(i) for i in hum]
 
+[os.remove(i) for i in csv_chunks]
 new.to_pickle(snakemake.output[0])
 
